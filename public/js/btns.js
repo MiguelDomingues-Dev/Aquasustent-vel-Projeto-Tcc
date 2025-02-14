d8dfd2f18346312f839b5a88cd747fb25723a3f2
MiguelDomingues-Dev <miguelcamargoadomingues@gmail.com>
2025-02-14 10:29:51 -0300

    acertando erros

--- public/js/btns.js
+++ public/js/btns.js
@@ -1,11 +1,11 @@
 document.querySelector("#btnLogin").addEventListener('click', function () {
-    window.location.href = "/pages/login.html"; // Remova "/pages/pages"
+    window.location.href = "/pages/pages/login.html"; // Remova "/pages/pages"
 });
 
 document.querySelector(".btnCadastrar").addEventListener('click', function () {
-    window.location.href = "/pages/cadastro.html";
+    window.location.href = "/pages/pages/cadastro.html";
 });
 
 document.querySelector("#btnCadastrar").addEventListener("click", function () {
-    window.location.href = "/pages/cadastro.html";
+    window.location.href = "/pages/pages/cadastro.html";
 });
